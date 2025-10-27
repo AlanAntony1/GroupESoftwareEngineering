4d20e1347b7a5e36932ce1f9fa5b18083805676b
AditiMahangade <aditi.a.mahangade-1@ou.edu>
2025-10-26 22:40:23 -0500

    aditi django

--- parkinglotlocater/models.py
+++ parkinglotlocater/models.py
@@ -11,3 +11,15 @@ class Building(models.Model):
     
     def getDistance(self):
         return self.distance
+
+# Aditi Model
+class Housing(models.Model):
+    housingName = models.CharField(max_length = 100)
+    closestParking = models.CharField(max_length = 100)
+    distance = models.DecimalField(max_digits = 5, decimal_places = 2, default = 0.0)
+
+    def __str__(self):
+        return self.housingName
+    
+    def getDistance(self):
+        return self.distance
