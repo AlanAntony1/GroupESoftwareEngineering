e5ddd0eb804ac4a9a321d06cfff0a1abf26e3b56
elizaglass18 <eanne.glass@gmail.com>
2025-10-30 11:04:50 -0500

    working on parkingLotHistory unit test

--- OUParking/settings.py
+++ OUParking/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'parkinglotlocater',
     'scheduleInput',
     'parkingLotHistory',
+    'parkingLotHistory.apps.ParkinglothistoryConfig',
 ]
 
 MIDDLEWARE = [
---
+++ parkingLotHistory/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+from .models import ParkingHistory
+
+# Register the model to appear in the admin site
+admin.site.register(ParkingHistory)
