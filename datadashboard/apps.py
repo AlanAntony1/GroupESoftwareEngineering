0ba8d6f8483564da6702e764651071857530949d
AnhNguyen2233 <tuan.anh.nguyen-1@ou.edu>
2025-10-30 22:11:42 -0500

    Fix merge leftovers in app configs

--- datadashboard/apps.py
+++ datadashboard/apps.py
@@ -1,12 +1,6 @@
 from django.apps import AppConfig
 
-
-<<<<<<<< HEAD:availableLots/apps.py
-class AvailablelotsConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'availableLots'
-========
 class DatadashboardConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'datadashboard'
->>>>>>>> feature/datadashboard:datadashboard/apps.py
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "datadashboard"
+    verbose_name = "Data Dashboard"
