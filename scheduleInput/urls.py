52ab2b0637b08b036f3eff2cce7ef78755cbdf08
jordanm05 <jordan.m.musselman@gmail.com>
2025-12-03 09:59:23 -0600

    Update to schedule path

--- scheduleInput/urls.py
+++ scheduleInput/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 
 urlpatterns = [
-    path("schedule/", views.schedule, name="schedule"),
+    path("", views.schedule, name="schedule"),
     path("delete/<int:pk>/", views.delete_class, name="delete_class"),
 ]
