3b62d938c3d21dc02c6f39b618c14b6722a83a8b
elizaglass18 <eanne.glass@gmail.com>
2025-12-04 17:47:29 -0600

    update models

--- parkingLotHistory/models.py
+++ parkingLotHistory/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.contrib.auth.models import User
+#from django.contrib.auth.models import User
 
 #class ParkingHistory(models.Model):
  #   lot_name = models.CharField(max_length=100)
@@ -10,17 +10,13 @@ from django.contrib.auth.models import User
     #def __str__(self):
     #    return f"{self.lot_name} at {self.timestamp}"
 
-def setUp(self):
-    super().setUp()
-    # Create a test user
-    user = User.objects.create(username="testuser")
-    # Add a test parking history
-    ParkingHistory.objects.create(
-        user=user,
-        lot_name="Lot A",
-        occupied_spots=5,
-        available_spots=10
-    )
+class ParkingHistory(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+    building_name = models.CharField(max_length=255, null=True, blank=True)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.building_name} @ {self.timestamp}"
 
 
 #class ParkingHistory(models.Model):
