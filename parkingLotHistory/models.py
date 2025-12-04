9de9cda8d697aa4188727e95884a97e381f80497
elizaglass18 <eanne.glass@gmail.com>
2025-12-04 17:33:29 -0600

    fixed

--- parkingLotHistory/models.py
+++ parkingLotHistory/models.py
@@ -1,14 +1,14 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-#class ParkingHistory(models.Model):
- #   lot_name = models.CharField(max_length=100)
-  #  occupied_spots = models.IntegerField()
-  #  available_spots = models.IntegerField()
-   # timestamp = models.DateTimeField(auto_now_add=True)
+class ParkingHistory(models.Model):
+    lot_name = models.CharField(max_length=100)
+    occupied_spots = models.IntegerField()
+    available_spots = models.IntegerField()
+    timestamp = models.DateTimeField(auto_now_add=True)
 
-    #def __str__(self):
-     #   return f"{self.lot_name} at {self.timestamp}"
+    def __str__(self):
+        return f"{self.lot_name} at {self.timestamp}"
 
 
 
