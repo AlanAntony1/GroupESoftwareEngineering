82d26facfdac007c3ffdeedfc6e201deaaba1523
AnhNguyen2233 <tuan.anh.nguyen-1@ou.edu>
2025-12-04 07:28:43 -0600

    hope it fixed this time :(

--- datadashboard/urls.py
+++ datadashboard/urls.py
@@ -7,7 +7,7 @@ app_name = "datadashboard"
 urlpatterns = [
     path("", views.home, name="dashboard-home"),      # /dashboard/
     path("data.json", views.data_json, name="dashboard-data"),
-    path("ping/", views.ping, name="ping"),           # /dashboard/ping/
+
 ]
 
 
