fc224e2fc6175d41a6503bf3236f180606f6721a
elizaglass18 <eanne.glass@gmail.com>
2025-12-04 20:42:34 -0600

    update history

--- parkingLotHistory/models.py
+++ parkingLotHistory/models.py
@@ -2,8 +2,6 @@ from django.db import models
 
 class ParkingHistory(models.Model):
     lot_name = models.CharField(max_length=100)
-    occupied_spots = models.IntegerField()
-    available_spots = models.IntegerField()
     timestamp = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
@@ -16,7 +14,6 @@ class HousingHistory(models.Model):
 
     def __str__(self):
         return f"{self.building_name} at {self.timestamp}"
-
 
 
 
