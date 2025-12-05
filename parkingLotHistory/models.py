6ecdb7a36caae6303f9b5256b6febbbc0badd55f
elizaglass18 <eanne.glass@gmail.com>
2025-12-04 20:15:22 -0600

    update history

--- parkingLotHistory/models.py
+++ parkingLotHistory/models.py
@@ -1,23 +1,23 @@
-#from django.db import models
-
-#class ParkingHistory(models.Model):
-#    lot_name = models.CharField(max_length=100)
- #   occupied_spots = models.IntegerField()
- #   available_spots = models.IntegerField()
- #   timestamp = models.DateTimeField(auto_now_add=True)
-
-#    def __str__(self):
-#        return f"{self.lot_name} at {self.timestamp}"
-
 from django.db import models
-from django.contrib.auth.models import User
 
 class ParkingHistory(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
-    building_name = models.CharField(max_length=255, null=True, blank=True)
+    lot_name = models.CharField(max_length=100)
+    occupied_spots = models.IntegerField()
+    available_spots = models.IntegerField()
     timestamp = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return f"{self.building_name} @ {self.timestamp}"
+        return f"{self.lot_name} at {self.timestamp}"
+
+#from django.db import models
+#from django.contrib.auth.models import User
+
+#class ParkingHistory(models.Model):
+    #user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+    #building_name = models.CharField(max_length=255, null=True, blank=True)
+    #timestamp = models.DateTimeField(auto_now_add=True)
+
+    #def __str__(self):
+   #     return f"{self.building_name} @ {self.timestamp}"
 
 
--- parkingLotHistory/urls.py
+++ parkingLotHistory/urls.py
@@ -1,16 +1,15 @@
-#from django.urls import path
-#from . import views
-
-#urlpatterns = [
- #   path('add_history/', views.add_history, name='add_history'),
-  #  path('history/', views.parking_history, name='parking_history'),
-#]
-
-
 from django.urls import path
 from . import views
 
 urlpatterns = [
     path('history/', views.parking_history, name='parking_history'),
-    path('add_history/', views.add_history, name='add_history'),
 ]
+
+
+#from django.urls import path
+#from . import views
+
+#urlpatterns = [
+ #   path('history/', views.parking_history, name='parking_history'),
+  #  path('add_history/', views.add_history, name='add_history'),
+#]
