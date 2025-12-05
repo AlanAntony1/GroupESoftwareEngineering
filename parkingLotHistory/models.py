a48a535e89f378545d1ec579f24094c803bfcd5a
elizaglass18 <eanne.glass@gmail.com>
2025-12-04 20:45:44 -0600

    fixing

--- parkingLotHistory/models.py
+++ parkingLotHistory/models.py
@@ -2,10 +2,13 @@ from django.db import models
 
 class ParkingHistory(models.Model):
     lot_name = models.CharField(max_length=100)
+    occupied_spots = models.IntegerField()
+    available_spots = models.IntegerField()
     timestamp = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return f"{self.lot_name} at {self.timestamp}"
+
 
 
 class HousingHistory(models.Model):
@@ -14,6 +17,7 @@ class HousingHistory(models.Model):
 
     def __str__(self):
         return f"{self.building_name} at {self.timestamp}"
+
 
 
 
