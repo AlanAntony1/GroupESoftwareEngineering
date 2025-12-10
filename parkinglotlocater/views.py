f074b974922a9b8e2d9480a1cfed9b9615268db5
Jason-SalinasOU <143210653+Jason-SalinasOU@users.noreply.github.com>
2025-12-09 20:26:07 -0600

    updating the views.py for parkinglotlocater to read in json

--- parkinglotlocater/views.py
+++ parkinglotlocater/views.py
@@ -1,11 +1,22 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import JsonResponse
 from .models import Building
+from HighlightButton.models import Highlight 
+import json
 
 # Create your views here.
 def building_view(request):
+    
+    ##Highlightbutton because we cant push two returns
+    highlighted = Highlight.objects.filter(isHighlighted=True)
+    highlighted_ids = [h.spotid for h in highlighted]
+    highlighted_json = json.dumps(highlighted_ids)
+    
     buildings = Building.objects.all()
-    return render(request, 'parkinglotlocater/locater.html', {"buildings": buildings})
+    return render(request, 'parkinglotlocater/locater.html', {
+        "highlighted_spots_json": highlighted_json,
+        "buildings": buildings,
+    })
 
 def building_details(request, building_id):
     building = get_object_or_404(Building, id = building_id)
